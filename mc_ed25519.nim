1bd68f5397462ae1d2cad4261f0fc7c4b093fea2
Luke Parker <lukep@embercrypto.io>
2019-06-02 17:19:46 -0500

    Replace sodiumKeyPair with multiplyBase and clean the wrapper.

--- mc_ed25519.nim
+++ mc_ed25519.nim
@@ -1,39 +1,38 @@
 #Include and link Sodium.
-const currentFolder = currentSourcePath().substr(0, currentSourcePath().len - 11)
+const currentFolder = currentSourcePath().substr(0, currentSourcePath().len - 15)
 {.passC: "-I" & currentFolder & "libsodium/".}
 {.passL: "-lsodium".}
 
-#---------- Ed25519 ----------
-
 #Define the Ed25519 objects.
 type
-    Ed25519State* {.
-        header: "sodium.h",
-        importc: "crypto_sign_ed25519ph_state"
-    .} = object
     Seed* = array[32, cuchar]
     PrivateKey* = array[64, cuchar]
     PublicKey* = array[32, cuchar]
+    State* {.
+        header: "sodium.h",
+        importc: "crypto_sign_ed25519ph_state"
+    .} = object
 
-#Sodium function for creating a key pair.
-func sodiumKeyPair*(
-    pub: ptr cuchar,
-    priv: ptr cuchar,
-    seed: ptr cuchar
-): int {.
+#Multiply a point in a char array against the base point and store it in the provided point.
+proc multiplyBase*(
+    output: ptr cuchar,
+    input: ptr cuchar
+) {.
     header: "sodium.h",
-    importc: "crypto_sign_ed25519_seed_keypair"
+    importc: "crypto_scalarmult_ed25519_base"
 .}
 
-#Sodium function for initiating a state.
-func sodiumInitState*(state: ptr Ed25519State): int {.
+#Init a state.
+proc init*(
+    state: ptr State
+): int {.
     header: "sodium.h",
     importc: "crypto_sign_ed25519ph_init"
 .}
 
-#Sodium function for updating a state.
-func sodiumUpdateState*(
-    state: ptr Ed25519State,
+#Update a state.
+proc update*(
+    state: ptr State,
     msg: ptr char,
     len: culong
 ): int {.
@@ -41,9 +40,9 @@ func sodiumUpdateState*(
     importc: "crypto_sign_ed25519ph_update"
 .}
 
-#Sodium function for signing a message.
-func sodiumSign*(
-    state: ptr Ed25519State,
+#Sign a message.
+proc sign*(
+    state: ptr State,
     sig: ptr char,
     len: ptr culong,
     priv: PrivateKey
@@ -52,9 +51,9 @@ func sodiumSign*(
     importc: "crypto_sign_ed25519ph_final_create"
 .}
 
-#Sodium function for verifying a message.
-func sodiumVerify*(
-    state: ptr Ed25519State,
+#Verify a message.
+proc verify*(
+    state: ptr State,
     sig: ptr char,
     pub: PublicKey
 ): int {.
