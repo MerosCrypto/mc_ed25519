42dc1bb74a2f24e8cd230a8d6c29cb5f85ef2fd9
Luke Parker <lukep@embercrypto.io>
2019-07-08 20:37:01 -0500

    Add enough functions to support MuSig key aggregation.

--- mc_ed25519.nim
+++ mc_ed25519.nim
@@ -22,31 +22,72 @@ extern "C" {
 
 #Define the Ed25519 objects.
 type
-    FE* {.
-        header: "fe.h",
-        importc: "fe"
-    .} = array[10, int32]
-    Point* {.
+    PointP1P1* {.
+        header: "ge.h",
+        importc: "ge_p1p1"
+    .} = object
+    Point2* {.
+        header: "ge.h",
+        importc: "ge_p2"
+    .} = object
+    Point3* {.
         header: "ge.h",
         importc: "ge_p3"
     .} = object
-        x: FE
-        y: FE
-        Z: FE
-        T: FE
+    PointCached* {.
+        header: "ge.h",
+        importc: "ge_cached"
+    .} = object
     PrivateKey* = array[64, cuchar]
     PublicKey* = array[32, cuchar]
 
+#Convert a Public Key to a Point3.
+proc keyToNegativePoint*(
+    res: ptr Point3,
+    bytes: ptr cuchar
+) {.importc: "ge_frombytes_negate_vartime".}
+
+proc p3ToCached*(
+    cached: ptr PointCached,
+    p3: ptr Point3
+) {.importc: "ge_p3_to_cached".}
+
+proc p1p1ToP3*(
+    p3: ptr Point3,
+    p1p1: ptr PointP1P1
+) {.importc: "ge_p1p1_to_p3".}
+
+#Add two points.
+proc add*(
+    res: ptr PointP1P1,
+    p: ptr Point3,
+    p2: ptr PointCached
+) {.importc: "ge_add".}
+
 #Multiply by Ed25519's base.
 proc multiplyBase*(
-    res: ptr Point,
+    res: ptr Point3,
     point: ptr cuchar
-) {.importc: "ge_scalarmult_base".}
+) {.importc: "ge_scalarmult".}
 
-#Serialize a point.
+#Multiply scalars.
+proc multiplyScalar*(
+    res: ptr Point2,
+    scalar: ptr cuchar,
+    point: ptr Point3,
+    addScalar: ptr cuchar
+) {.importc: "ge_double_scalarmult_vartime".}
+
+#Serialize a Point2.
 proc serialize*(
     res: ptr cuchar,
-    point: ptr Point
+    point2: ptr Point2
+) {.importc: "ge_tobytes".}
+
+#Serialize a Point3.
+proc serialize*(
+    res: ptr cuchar,
+    point: ptr Point3
 ) {.importc: "ge_p3_tobytes".}
 
 #Sign a message.
